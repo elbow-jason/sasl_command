bc69902076e98b67aa1f73671f9ea0c8fe6b9e1c
Jason Goldberger <jlgoldb2@asu.edu>
2016-01-01 15:00:49 -0700

    added request plain

--- lib/sasl_command.ex
+++ lib/sasl_command.ex
@@ -1,15 +1,53 @@
 defmodule SaslCommand do
   require SaslEx
 
-  @magic_request           0x80
-  @magic_response          0x81
-  @opcode_list_mechanism   0x20
+  @magic_request            128 # 0x80
+  @magic_response           129 # 0x81
+
+  @plain                    "PLAIN"
+  @cram_md5                 "CRAM-MD5"
+  @plain_and_cram_md5       "#{@plain} #{@cram_md5}"
+
+  @opcode_list_mechanism    32  # 0x20
+  @opcode_auth              33  # 0x21
+
+  @status_success           0 # 0x0000
+
 
   def list_mechanism do
     %SaslEx{magic: @magic_request, opcode: @opcode_list_mechanism}
   end
 
+  def list_mechanism_response_plain_cram_md5 do
+    %SaslEx{
+      magic:      @magic_response,
+      opcode:     @opcode_list_mechanism,
+      total_body: String.length(@plain_and_cram_md5),
+      payload:    @plain_and_cram_md5,
+    }
+  end
 
+  def list_mechanism_response_plain do
+    %SaslEx{
+      magic:      @magic_response,
+      opcode:     @opcode_list_mechanism,
+      total_body: String.length(@plain),
+      payload:    @plain,
+    }
+  end
 
+  def request_plain(auth_token) when auth_token |> is_binary do
+    if byte_size(auth_token) != 10 do
+      raise "SaslCommand.request_plain: auth_token must be exactly 10 bytes long"
+    end
+    payload = @plain <> auth_token
+    %SaslEx{
+      magic:      @magic_request,
+      opcode:     @opcode_auth,
+      key_length: 5,
+      total_body: String.length(payload),
+      payload:    payload,
+    }
+  end 
 
 end
--- test/sasl_command_test.exs
+++ test/sasl_command_test.exs
@@ -4,6 +4,36 @@ defmodule SaslCommandTest do
   alias SaslEx
 
   test "list mechanism" do
-    assert SaslCommand.list_mechanism == %SaslEx{magic: 0x80, opcode: 0x20}
+    assert SaslCommand.list_mechanism == %SaslEx{magic: 128, opcode: 32}
   end
+
+  test "list mechanism response plain" do
+    expected = %SaslEx{magic: 129, opcode: 32, total_body: 5, payload: "PLAIN"}
+    assert SaslCommand.list_mechanism_response_plain == expected
+  end
+
+  test "list mechanism response plain_and_cram_md5" do
+    expected = %SaslEx{magic: 129, opcode: 32, total_body: 14, payload: "PLAIN CRAM-MD5"}
+    assert SaslCommand.list_mechanism_response_plain_cram_md5 == expected
+  end
+
+  test "plain_request auth_token wrong size raises error" do
+    err = "SaslCommand.request_plain: auth_token must be exactly 10 bytes long"
+    assert_raise RuntimeError, err, fn ->
+      SaslCommand.request_plain("too_short")
+    end
+  end
+
+  test "plain_request works with correctly sized token" do
+    expected = %SaslEx{
+      key_length: 5,
+      magic: 128,
+      opcode: 33,
+      payload: "PLAINjust_right",
+      total_body: 15
+    }
+    assert SaslCommand.request_plain("just_right") == expected
+  end
+
+
 end
